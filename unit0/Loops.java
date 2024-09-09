f7faa22f3e3ca1de1206ed4f391af286c87f42f5
SamiS0830 <samanthashapiro27@marlborough.org>
2024-09-09 11:52:54 -0700

    HW

---
+++ unit0/Loops.java
@@ -0,0 +1,18 @@
+public class Loops
+{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<30;i++)
+        {
+            if(i%2 = 0)
+            {
+                System.out.println("Even");
+            }
+            else if
+            {
+                 System.out.println(i);
+            }
+           
+        }
+    }
+}
