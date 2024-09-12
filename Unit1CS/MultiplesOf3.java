dfb3aa6b73f2e477e4b54ae509e98a848ecda94b
SamiS0830 <samanthashapiro27@marlborough.org>
2024-09-11 22:27:28 -0700

    Fizzbuzz - HW

---
+++ Unit1CS/MultiplesOf3.java
@@ -0,0 +1,10 @@
+public class MultiplesOf3 {
+ 
+   public static void main(String[] args) {
+       for(int i = 0; i<30; i+=3)
+       {
+       System.out.println(i);
+       }
+      System.out.println("Hello World!");
+   }
+}   // end of class HelloWorld
