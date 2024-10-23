245a7f0b1b945e0fdf2dafec31655fa8cad007a8
SamiS0830 <samanthashapiro27@marlborough.org>
2024-10-23 09:47:20 -0700

    HW - Unit 5

---
+++ Unit5CS/CoinFlipper.java
@@ -0,0 +1,13 @@
+public class CoinFlipper(){
+    public Coin(){   
+    }
+    public void flip{
+        double a = Math.random();
+        if(a<0.5){
+            return "heads";
+        }
+        else(){
+            return "tails";
+        }
+    }
+}
