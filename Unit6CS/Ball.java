4891929794fb099c6fa0fb7bba8fc5f5f95dad1e
SamiS0830 <samanthashapiro27@marlborough.org>
2024-11-03 20:40:12 -0800

    HW

---
+++ Unit6CS/Ball.java
@@ -0,0 +1,52 @@
+
+import processing.core.PApplet;
+
+public class Ball extends PApplet{
+    
+
+    public void settings(){
+        size(400,400);
+    }
+    public void setup(){
+        draw();
+   
+        x = width/2;
+        y = width/2;
+        vx = 1;
+        vy = 1;
+         r = 5;
+       
+    }
+    public void draw(){
+    background(255);
+    checkEdges();
+    x+=vx;
+    y+=vy;
+    fill(0);
+    ellipse(x,y,r*2,r*2);
+    }
+    public void checkEdges(){
+         if(x<0||x>400){
+            vx*=-1;
+        }
+        if(y<0||y>400){
+            vy*=-1;
+        }
+    }
+    public void keyPressed(){
+        if(keyCode == UP){
+            r+=2;
+        }
+    }
+    
+    public static void main (String[] args){
+    PApplet.main("Ball");
+    }
+    private float x;
+    private float y;
+    private float vy;
+    private float vx;
+    private float r;
+           
+
+}
