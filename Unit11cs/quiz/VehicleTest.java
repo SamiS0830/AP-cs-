a5f3804e7de360fc48d7a044ade3541eef939511
SamiS0830 <samanthashapiro27@marlborough.org>
2025-01-27 15:18:48 -0800

    HW

---
+++ Unit11cs/quiz/VehicleTest.java
@@ -0,0 +1,12 @@
+public class VehicleTest{
+    public static main void(String[] args){
+        ArrayList<Vehicle> vehicle = new ArrayList<Vehicle>();
+        vehicle.add(new Car());
+    }
+    public static int wheelCount(ArrayList<Vehicle> vehicle){
+       int return = 0;
+        for(Vehicle v : vehicle){
+            result+=wheelCount();
+        }
+    }
+}
