edc90ec524378dc9d7509542e9d984ff903463a3
SamiS0830 <samanthashapiro27@marlborough.org>
2025-02-06 09:38:55 -0800

    Project

---
+++ MyProject/Project.java
@@ -0,0 +1,24 @@
+
+import processing.core.*;
+
+public class Project extends PApplet{
+    public void settings(){
+        fullScreen();
+    }
+    
+    public void setup(){
+    startscene = new StartScene(this);
+    }
+    
+    public void draw(){
+        startscene.display();
+    }
+    
+    
+    public static void main(String[] args){
+        PApplet.main("Project");
+    }
+    private StartScene startscene;
+    
+    
+}
---
+++ MyProject/StartScene.java
@@ -0,0 +1,21 @@
+import processing.core.*;
+public class StartScene{
+    
+    public StartScene(PApplet p){
+        this.p = p;
+    }
+    
+    public void settings() {
+        
+    }
+    
+    public void setup(){
+
+        
+    }
+    public void display(){
+        p.background(0);
+
+    }
+    private PApplet p;
+}
